de5924a24a82265f28365ce8e53ebe6f4df353d6
Sabin <50198431+sabin5105@users.noreply.github.com>
2019-10-25 16:47:39 +0900

    Add files via upload

---
+++ C++/9000~10000/9375.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	int k;
+	cin >> k;
+	for (int i = 0; i < k; i++)
+	{
+		int n;
+		cin >> n;
+		string str[20];
+		for (int j = 0; j < n; j++)
+		{
+			cin >> str[j];
+		}
+	}
+}
