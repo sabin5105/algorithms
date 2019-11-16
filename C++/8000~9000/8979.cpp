082e590b34ac29175888c2b9a363d83e7a476783
Sabin <50198431+sabin5105@users.noreply.github.com>
2019-11-16 21:57:05 +0900

    Update 8979.cpp

--- C++/8000~9000/8979.cpp
+++ C++/8000~9000/8979.cpp
@@ -1,17 +1,35 @@
 #include<iostream>
 #include<algorithm>
-
 using namespace std;
+
+int n, k, ans, a, b, c;
+int arr[1005][4];
+
 int main()
 {
-	int n, k, cnt=0;
 	cin >> n >> k;
 
-	int arr[1000][4];
-	for (int i = 0; i < n; i++)
+	for (int i = 1; i <= n; i++)
+	{
 		for (int j = 0; j < 4; j++)
+		{
 			cin >> arr[i][j];
+			if (arr[i][0] == k)
+			{
+				a = arr[i][1];
+				b = arr[i][2];
+				c = arr[i][3];
+			}
+		}
+	}
 
-	
+	ans = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		if (arr[i][1] > a) ans++;
+		else if (arr[i][1] == a && arr[i][2] > b) ans++;
+		else if (arr[i][1] == a && arr[i][2] == b && arr[i][3] > c) ans++;
+	}
+	cout << ans;
+		
 }
-// unsolved
