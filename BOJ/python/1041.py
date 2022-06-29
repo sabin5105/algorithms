96bcb4197a14a2e99b5cdd44a4634f764e960e02
Sabin <50198431+sabin5105@users.noreply.github.com>
2022-06-29 23:21:16 +0900

    Update 1041.py

--- BOJ/python/1041.py
+++ BOJ/python/1041.py
@@ -16,11 +16,15 @@ if __name__ == '__main__':
         min_lists.sort()
 
         min1 = min_lists[0]
-        min2 = min_lists[0] + min_lists[1]
-        min3 = sum(min_lists)
+        min2 = min_lists[1]
+        min3 = min_lists[2]
 
         n1 = 4 * (N - 2) * (N - 1) + (N - 2) ** 2
         n2 = 4 * (N - 1) + 4 * (N - 2)
+        n3 = 4
+
+        n1 = 5*(N**2) - 8*N + 4
+        n2 = 8*N - 8
         n3 = 4
 
         ans += min1 * n1
